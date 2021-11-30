9fb0d7dbdc5639cbd936014a863212901d75a076
jacksonbates <jacksonbates@hotmail.com>
2021-11-30 16:08:44 +1100

    wip2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./Dashboard";
 import LessonScreen1 from "./LessonScreen1";
 import LessonScreen2 from "./LessonScreen2";
